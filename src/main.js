3a87fb6029ade44dfdeb8b09892a43145c534c72
Daniel Douglas <dan.douglas.01@gmail.com>
2020-07-05 17:29:12 +0100

    chore(app): add firori fundamentals lib

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FundamentalVue from 'fundamental-vue';
 
 Vue.config.productionTip = false
+Vue.use(FundamentalVue);
 
 new Vue({
   render: h => h(App),
