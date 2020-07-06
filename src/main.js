7cf045a8aa933a85ee1284e9024988b299fb8d2c
Daniel Douglas <dan.douglas.01@gmail.com>
2020-07-06 21:23:23 +0100

    task(filter): capitalize

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import FundamentalVue from 'fundamental-vue';
 Vue.config.productionTip = false
 Vue.use(FundamentalVue);
 
+Vue.filter('capitalize', function (value) {
+  if (!value) return ''
+  value = value.toString()
+  return value.charAt(0).toUpperCase() + value.slice(1)
+})
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
