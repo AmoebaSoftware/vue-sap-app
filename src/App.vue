42c3c5d98e6eed43ea2cba1e8953dfbda9a22368
Daniel Douglas <dan.douglas.01@gmail.com>
2020-07-08 20:41:30 +0100

    task(app): consume odata api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ProductTable v-bind:products="products"/>
+    <ProductTable v-bind:products="products" v-if="!isLoading"/>
   </div>
 </template>
 
 <script>
 import ProductTable from './components/ProductTable.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -15,27 +15,15 @@ export default {
   },
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: 'Test Name 1',
-          description: 'Test Description 1',
-          rating: 1
-        },
-        {
-          id: 2,
-          name: 'Test Name 2',
-          description: 'Test Description 2',
-          rating: 2
-        },
-        {
-          id: 3,
-          name: 'Test Name 3',
-          description: 'Test Description 3',
-          rating: 3
-        }
-      ]
+      isLoading: true,
+      products: null
     }
+  },
+  mounted () {
+    axios
+      .get('https://services.odata.org/Experimental/OData/OData.svc/Products')
+      .then(response => (this.products = response.data.value))
+      .then(() => this.isLoading = false)
   }
 }
 </script>
@@ -52,4 +40,8 @@ export default {
 </style>
 <style lang='scss'>
   @import "./scss/main.scss";
+
+  pre {
+    text-align: left;
+  }
 </style>
