ff8f18d82f27504cfab9b22b017388064c7901c4
Daniel Douglas <dan.douglas.01@gmail.com>
2020-07-11 10:10:52 +0100

    task(app): header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="app">
-    <ProductTable v-bind:products="products" v-if="!isLoading"/>
+    <fd-shell-bar>
+      <fd-shell-bar-group position="start">
+        <fd-shell-bar-logo alt="SAP" src="'assets/sap-logo.png'" width="48" height="24" />
+        <fd-shell-bar-product productTitle="Product Name" />
+      </fd-shell-bar-group>
+      <fd-shell-bar-group position="end">
+        <fd-shell-bar-actions>
+          <fd-shell-bar-action>
+            <fd-shell-bar-user-menu ref="userMenu">
+              <fd-menu>
+                <fd-menu-list>
+                  <fd-menu-item @click="closeUserMenu">
+                    Settings
+                  </fd-menu-item>
+                  <fd-menu-item @click="closeUserMenu">
+                    Sign out
+                  </fd-menu-item>
+                </fd-menu-list>
+              </fd-menu>
+            </fd-shell-bar-user-menu>
+          </fd-shell-bar-action>
+        </fd-shell-bar-actions>
+      </fd-shell-bar-group>
+    </fd-shell-bar>
+    <fd-container centered>
+      <ProductTable v-bind:products="products" v-if="!isLoading"/>
+    </fd-container>
   </div>
 </template>
 
@@ -25,24 +51,27 @@ export default {
       .then(response => (this.products = response.data.value))
       .then(() => this.isLoading = false)
       .catch((err) => console.error(err))
-  }
+  },
+  methods: {
+    closeUserMenu() {
+      this.$refs.userMenu.close();
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
 <style lang='scss'>
   @import "./scss/main.scss";
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-  pre {
-    text-align: left;
+  table {
+    margin-top: 3rem;
   }
 </style>
+
